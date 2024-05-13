3ff30fadf2a358d13e28ccb991ae448cc00ea043
yasinarafat4870 <yasinarafat4870@gmail.com>
2024-05-13 18:24:16 +0600

    Banner Part Rsponsive Done

--- src/Component/HomeComponent/Banner/Banner.jsx
+++ src/Component/HomeComponent/Banner/Banner.jsx
@@ -36,6 +36,7 @@ const Banner = () => {
           width: "30px",
           color: "var(--main-font-color)",
           borderRight: "2px solid var(--main-bg-color)",
+          borderRight: "red",
           fontSize: "12px",
           padding: "10px 0",
         }}
@@ -43,11 +44,118 @@ const Banner = () => {
         0{i + 1}
       </div>
     ),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          appendDots: (dots) => (
+            <div
+              style={{
+                backgroundColor: "transparent",
+                borderRadius: "10px",
+                padding: "10px",
+                position: "absolute",
+                top: "96%",
+                left: "50%",
+                transform: "translateY(-50%)",
+              }}
+            >
+              <ul style={{ margin: "0px", transform: "rotate(90deg)" }}>
+                {dots}
+              </ul>
+            </div>
+          ),
+          customPaging: (i) => (
+            <div
+              style={{
+                width: "15px",
+                borderRight: "2px solid #B0EBB4",
+                fontSize: "9px",
+                padding: "10px 0",
+              }}
+            ></div>
+          ),
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+          appendDots: (dots) => (
+            <div
+              style={{
+                backgroundColor: "transparent",
+                borderRadius: "10px",
+                padding: "10px",
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translateY(-50%)",
+              }}
+            >
+              <ul style={{ margin: "0px", transform: "rotate(90deg)" }}>
+                {dots}
+              </ul>
+            </div>
+          ),
+          customPaging: (i) => (
+            <div
+              style={{
+                width: "15px",
+                borderRight: "2px solid #B0EBB4",
+                fontSize: "9px",
+                padding: "10px 0",
+              }}
+            ></div>
+          ),
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          appendDots: (dots) => (
+            <div
+              style={{
+                backgroundColor: "transparent",
+                borderRadius: "10px",
+                padding: "10px",
+                position: "absolute",
+                top: "93%",
+                left: "50%",
+                transform: "translateY(-50%)",
+              }}
+            >
+              <ul style={{ margin: "0px", transform: "rotate(90deg)" }}>
+                {dots}
+              </ul>
+            </div>
+          ),
+          customPaging: (i) => (
+            <div
+              style={{
+                width: "15px",
+                borderRight: "2px solid #9DDE8B",
+                fontSize: "9px",
+                padding: "10px 0",
+              }}
+            ></div>
+          ),
+        },
+      },
+    ],
   };
 
   return (
     <>
-      <div className=" overflow-hidden">
+      <div className="overflow-hidden">
         <Slider {...settings}>
           <div>
             <picture>
