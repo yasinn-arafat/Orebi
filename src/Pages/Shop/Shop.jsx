c94d595f8df4afc860ee662f069d7b4efa20f612
yasinn-arafat <yasinarafat4870@gmail.com>
2024-07-16 00:28:14 +0600

    Product Deatails Component added

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import BreadCrumb from "../../Component/CommonComponent/BreadCrumb/BreadCrumb";
+import CommonHeading from "../../Component/CommonComponent/CommonHeading/CommonHeading";
 import ShopLeft from "../../Component/ShopComponent/ShopLeft/ShopLeft";
 import ShopRight from "../../Component/ShopComponent/ShopRight/ShopRight";
 
@@ -9,12 +9,7 @@ const Shop = () => {
       <div className="py-14">
         <div className="container">
           <div>
-            <div className="pb-6">
-              <h2 className="font-DMsans text-5xl font-bold text-main-font-color">
-                Products
-              </h2>
-            </div>
-            <BreadCrumb />
+            <CommonHeading title={"Products"} />
           </div>
           <div className="flex pt-20">
             <ShopLeft className={"w-[23%]"} />
