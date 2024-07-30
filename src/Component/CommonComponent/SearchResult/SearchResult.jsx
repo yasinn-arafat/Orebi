6ab5b54d19a306c5dda2296a65e4fdc0221c89d7
yasinn-arafat <yasinarafat4870@gmail.com>
2024-07-30 22:27:43 +0600

    Search result Modified

--- src/Component/CommonComponent/SearchResult/SearchResult.jsx
+++ src/Component/CommonComponent/SearchResult/SearchResult.jsx
@@ -7,7 +7,7 @@ const SearchResult = ({ className, searchResult, onToProduct }) => {
         {searchResult?.map((product) => (
           <div
             onClick={() => onToProduct(product.id)}
-            className="h-full max-w-[600px] cursor-pointer rounded-sm border-b-2 border-b-gray-300 bg-gray-200 px-3 py-2 hover:bg-gray-400 sm:w-[400px] md:w-[450px] lg:w-[550px]"
+            className="h-full max-w-[600px] cursor-pointer rounded-sm border-b-2 border-b-gray-300 bg-gray-200 px-3 py-2 hover:bg-gray-400 lg:w-[550px]"
             key={product.id}
           >
             <span>{product.title}</span>
